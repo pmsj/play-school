86662a74047c261bac8ef76f776bb1dd375cfac0
Prawin Minj <prawinajay@gmail.com>
2025-03-20 03:56:16 +0530

    modified article show page

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,64 @@
         </x-slot>
         <div class="my-10 mx-auto max-w-7xl p-5">
             <div class="grid lg:grid-cols-12">
-                <div class="lg:col-span-3 h-96">
+                <div class="lg:col-span-3 h-96 bg-blue-50 rounded-md">
+                    <!-- drop down links -->
+                    <div>
+                        <x-wui-dropdown>
+                            <x-slot name="trigger">
+                                <x-wui-button label="Account Settings" flat class="bg-slate" icon="user-circle" position="top-start" />
+                            </x-slot>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.info') }}" :active="request()->routeIs('profile.info')">
+                                    {{ __('Profile') }}
+                                </x-responsive-nav-link>
+                            </div>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.password') }}" :active="request()->routeIs('profile.password')">
+                                    {{ __('Password') }}
+                                </x-responsive-nav-link>
+                            </div>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.security') }}" :active="request()->routeIs('profile.security')">
+                                    {{ __('Security') }}
+                                </x-responsive-nav-link>
+                            </div>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.delete') }}" :active="request()->routeIs('profile.delete')">
+                                    {{ __('Delete Account') }}
+                                </x-responsive-nav-link>
+                            </div>
+                      
+                        </x-wui-dropdown>
+                    </div>
+                    <div>
+                        <x-wui-dropdown>
+                            <x-slot name="trigger">
+                                <x-wui-button label="Account Settings" flat class="bg-slate" icon="user-circle" position="top-start" />
+                            </x-slot>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.info') }}" :active="request()->routeIs('profile.info')">
+                                    {{ __('Profile') }}
+                                </x-responsive-nav-link>
+                            </div>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.password') }}" :active="request()->routeIs('profile.password')">
+                                    {{ __('Password') }}
+                                </x-responsive-nav-link>
+                            </div>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.security') }}" :active="request()->routeIs('profile.security')">
+                                    {{ __('Security') }}
+                                </x-responsive-nav-link>
+                            </div>
+                            <div class="pt-1 pb-3">
+                                <x-responsive-nav-link href="{{ route('profile.delete') }}" :active="request()->routeIs('profile.delete')">
+                                    {{ __('Delete Account') }}
+                                </x-responsive-nav-link>
+                            </div>
+                      
+                        </x-wui-dropdown>
+                    </div>
                     <div>
                         <x-wui-dropdown>
                             <x-slot name="trigger">
--- resources/views/livewire/all-articles.blade.php
+++ resources/views/livewire/all-articles.blade.php
@@ -7,7 +7,7 @@
 <div class="flex flex-col py-12 space-y-6">
     <div>
         <!-- left area in 2column grid layout -->
-        <div class="mx-auto max-w-7xl grid lg:grid-cols-[0.5fr_2fr] gap-2 p-5 relative">
+        <div class="mx-auto max-w-7xl grid lg:grid-cols-[1fr_2fr] gap-2 p-5 relative">
             <!-- top most div on left -->
             <div class="justify-start ticky top-0 z-10">
                 <div class="lg:cols-span-3 flex flex-col space-y-2  mr-16 text-center">
@@ -51,14 +51,15 @@
                 </div>
                 <!-- bottom div -->
                 @foreach($articles as $article)
-                    <a href="{{ route('article.show', $article->slug)}}">
                         <div class="my-5">
                         <div class="space-y-2 p-5 bg-negative rounded-2xl shadow-sm">
-                            <div class="bg-slate-500 opacity-20 h-28 rounded-t-2xl">
-                                <img src="https://unsplash.com/photos/a-tall-blue-building-with-a-clock-on-its-side-fWBZpKTQ_4U" alt="" srcset="">
-                            </div>
-                                    <livewire:tags />
-                            <div class="text-xl font-extrabold">{{ $article->title }}</div>
+                            <a href="{{ route('article.show', $article->slug)}}">
+                                <div class="bg-slate-500 opacity-20 h-28 rounded-t-2xl">
+                                    <img src="https://unsplash.com/photos/a-tall-blue-building-with-a-clock-on-its-side-fWBZpKTQ_4U" alt="" srcset="">
+                                </div>
+                                        <livewire:tags />
+                                <div class="text-xl font-extrabold hover:underline">{{ $article->title }}</div>
+                            </a>
                             <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit placeat necessitatibus iusto quae velit adipisci reiciendis earum atque odio quos provident molestiae temporibus eius neque modi cum, sint quis fugiat!</div>
                             <!-- user infos -->
                             <div class="flex justify-between items-center">
@@ -76,7 +77,6 @@
                                 <div class="text-sm">Messages</div>
                             </div>
                         </div>
-                    </a>
                 @endforeach
                 </div>
             </div>
